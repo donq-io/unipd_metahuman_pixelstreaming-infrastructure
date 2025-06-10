776c418acbce777110beeeb255143dafef47c917
Marco N <marco.necci@donq.io>
2025-06-10 16:23:18 +0200

    change hardcoded address

--- Frontend/implementations/react/src/components/App.tsx
+++ Frontend/implementations/react/src/components/App.tsx
@@ -19,7 +19,7 @@ export const App = () => {
                 initialSettings={{
                     AutoPlayVideo: true,
                     AutoConnect: true,
-                    ss: "https://metahuman-production.unipd.cloud/signal-init",
+                    ss: "https://metahuman-production.unipd.cloud",
                     StartVideoMuted: true,
                     HoveringMouse: true,
                     WaitForStreamer: true
