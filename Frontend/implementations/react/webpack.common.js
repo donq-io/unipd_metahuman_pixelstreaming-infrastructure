70a61dabb25941b749e9e97a09ac74d20cbd2773
Marco N <marco.necci@donq.io>
2025-06-10 13:12:53 +0200

    slightly change webpack config hoping that it works

--- Frontend/implementations/react/webpack.common.js
+++ Frontend/implementations/react/webpack.common.js
@@ -6,75 +6,77 @@ const webpack = require('webpack');
 const fs = require('fs');
 
 const pages = fs.readdirSync('./src', { withFileTypes: true })
-	.filter(item => !item.isDirectory())
-	.filter(item => path.parse(item.name).ext === '.html')
-	.map(htmlFile => path.parse(htmlFile.name).name);
+  .filter(item => !item.isDirectory())
+  .filter(item => path.parse(item.name).ext === '.html')
+  .map(htmlFile => path.parse(htmlFile.name).name);
 
 module.exports = {
-	entry: pages.reduce((config, page) => {
-		config[page] = `./src/${page}.tsx`;
-		return config;
-	}, {}),
+  entry: pages.reduce((config, page) => {
+    config[page] = `./src/${page}.tsx`;
+    return config;
+  }, {}),
 
-    plugins: [
-        // Define environment variables for browser
-        new webpack.DefinePlugin({
-            'process.env.REACT_APP_SIGNALLING_SERVER_URL': JSON.stringify(process.env.REACT_APP_SIGNALLING_SERVER_URL || 'ws://localhost:80')
-        })
-    ].concat(pages.map((page) => new HtmlWebpackPlugin({
-          title: `${page}`,
-          template: `./src/${page}.html`,
-          filename: `${page}.html`,
-          chunks: [page],
-    }), )),
+  plugins: [
+    // Define environment variables for browser
+    new webpack.DefinePlugin({
+      'process.env': { 
+        REACT_APP_SIGNALLING_SERVER_URL: JSON.stringify(process.env.REACT_APP_SIGNALLING_SERVER_URL || 'ws://localhost:80') 
+        }
+    })
+  ].concat(pages.map((page) => new HtmlWebpackPlugin({
+    title: `${page}`,
+    template: `./src/${page}.html`,
+    filename: `${page}.html`,
+    chunks: [page],
+  }),)),
 
-    module: {
-      rules: [
-        {
-          test: /\.tsx?$/,
-          loader: 'ts-loader',
-          exclude: [
-            /node_modules/,
-          ],
-        },
-        {
-          test: /\.html$/i,
-          use: 'html-loader'
-        },
-        {
-          test: /\.css$/,
-          type: 'asset/resource',
-          generator: {
-            filename: 'css/[name][ext]'
-          }
-        },
-        {
-          test: /\.(png|svg)$/i,
-          type: 'asset/resource',
-          generator: {
-            filename: 'images/[name][ext]'
-          }
-        },
-      ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: [
+          /node_modules/,
+        ],
+      },
+      {
+        test: /\.html$/i,
+        use: 'html-loader'
+      },
+      {
+        test: /\.css$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'css/[name][ext]'
+        }
+      },
+      {
+        test: /\.(png|svg)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name][ext]'
+        }
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.svg', '.json'],
+  },
+  output: {
+    filename: '[name].js',
+    library: 'epicgames-react-frontend',
+    libraryTarget: 'umd',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    globalObject: 'this',
+    hashFunction: 'xxhash64',
+  },
+  experiments: {
+    futureDefaults: true
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, '../../../SignallingWebServer/Public'),
     },
-    resolve: {
-      extensions: ['.tsx', '.ts', '.js', '.svg', '.json'],
-    },
-    output: {
-      filename: '[name].js',
-      library: 'epicgames-react-frontend',
-      libraryTarget: 'umd',
-      path: path.resolve(__dirname, 'dist'),
-      clean: true,
-      globalObject: 'this',
-      hashFunction: 'xxhash64',
-    },
-    experiments: {
-      futureDefaults: true
-    },
-	devServer: {
-    	static: {
-    		directory: path.join(__dirname, '../../../SignallingWebServer/Public'),
-    	},
-    },
+  },
 }
